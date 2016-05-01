97402c70871e400fd9850c95aaf954666552c9c7
alviandk <alviandk@gmail.com>
2016-05-01 20:43:23 +0700

    fix product filtering

--- inventory/filters.py
+++ inventory/filters.py
@@ -3,8 +3,8 @@ from django_filters import FilterSet, CharFilter, NumberFilter
 from .models import Product
 
 class ProductFilter(FilterSet):
-	color = CharFilter(name='color', lookup_type='icontains', distinct=True)
-	size = CharFilter(name='soze', lookup_type='icontains', distinct=True)
+	color = CharFilter(name='variation__color', lookup_type='icontains', distinct=True)
+	size = CharFilter(name='variation__size', lookup_type='icontains', distinct=True)
 	min_price = NumberFilter(name='variation__price', lookup_type='gte', distinct=True)
 	max_price = NumberFilter(name='variation__price', lookup_type='lte', distinct=True)
 	class Meta:
@@ -13,5 +13,5 @@ class ProductFilter(FilterSet):
 			'min_price',
 			'max_price',
             'size',
-            'color',						
+            'color',
 		)
